f935c977d3ca65bb8021d6b76234e2286b1cbf30
laura1513 <laurajorques2003@gmail.com>
2023-09-22 09:04:47 +0200

    ejercicio2

--- src/ejercicio2.java
+++ src/ejercicio2.java
@@ -3,22 +3,26 @@ import java.util.Scanner;
 public class ejercicio2 {
     public static void main(String[] args) {
         Scanner read =  new Scanner(System.in);
-        String cadena = "";
+        int numero;
+        do {
+            System.out.print("Introduce un número mayor que '1': ");
+            numero = read.nextInt();
+        } while (numero < 1);
+        System.out.print(numero + "=");
+        factorizarPrimo(numero);
+    }
+    public static void factorizarPrimo(int numero) {
         int divisor = 2;
-
-        System.out.print("Introduce un número: ");
-        int numero = read.nextInt();
-
-        cadena = numero + " = ";
-
-        while (numero != 1) {
+        while (numero > 1) {
             if (numero % divisor == 0) {
-                cadena += divisor + " ";
+                System.out.print(divisor);
                 numero = numero / divisor;
+                if (numero > 1) {
+                    System.out.print("*");
+                }
             } else {
                 divisor++;
             }
         }
-        System.out.println(cadena);
     }
 }
